f3804aa7befe21948c457be095a4ae21df1d83d7
vermaharsh63 <75084132+vermaharsh63@users.noreply.github.com>
2021-01-24 16:21:37 +0530

    Add files via upload

---
+++ InventoryItem.java
@@ -0,0 +1,12 @@
+package homeinventory;
+
+
+/**
+ * Write a description of InventoryItem here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class InventoryItem {
+
+}
